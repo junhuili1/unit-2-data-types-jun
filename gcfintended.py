3116fa61fcec2d0d8b35badd2cfe0baef6b127a3
junhuili1 <junhuil6@nycstudents.net>
2026-09-25 09:17:47 -0400

    unit 2 final

--- gcfintended.py
+++ gcfintended.py
@@ -4,6 +4,7 @@ number2 = int(input("Enter the second number."))
 
 factors1 = []
 factors2 = []
+commonFactors = []
 
 for i in range(1, number1 + 1):
     if number1 % i == 0:
@@ -13,3 +14,11 @@ for i in range(1, number2 + 1):
     if number2 % i == 0:
         factors2.append(i)
 
+x = 0
+for i in range(len(factors1)):
+    for i in range(len(factors2)):
+        if factors1[x] == factors2[i]:
+            commonFactors.append(factors1[x])
+    x+=1
+
+print(f"The gcf of {number1} and {number2} is {commonFactors[-1]}.")
